46d7bb23059f133f3ca83f024d2d211f11fa1f60
Mandillah123* <official1337h4x0r@gmail.com>
2023-03-18 23:59:29 +0530

    hbchbh

--- generate.js
+++ generate.js
@@ -1,3 +1,8 @@
 generate = () => {
   let otp = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+  document.getElementById('otp').value = otp;
+}
+showHide = () => {
+  let otp = document.getElementById('otp').typ
+
 }

e2eb04191136cfb6c6dbe930bdbfd29bd439451e
Mandillah123* <official1337h4x0r@gmail.com>
2023-03-19 21:23:37 +0530

    otp and validation : basic example, Using querySelectorAll

--- generate.js
+++ generate.js
@@ -1,8 +1,16 @@
 generate = () => {
-  let otp = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+  var otp = Math.floor(Math.random() * (9999 - 1000)) + 1000;
   document.getElementById('otp').value = otp;
 }
 showHide = () => {
-  let otp = document.getElementById('otp').typ
-
+  var passwordFields = document.querySelectorAll('input[type=password]');
+  passwordFields.forEach(passwordField => {
+    var state = passwordField.type == "password" ? "text" : "password";
+    passwordField.type = state;
+  });
+}
+validate = () => {
+  let userOTP = document.getElementById('user-otp').value
+  let isValid = otp === userOTP;
+  alert(`OTP  ${isValid}`);
 }
